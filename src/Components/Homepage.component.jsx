5bdbd1dea7e843476dad6793831fcb0a8b02c977
jagtapmv <jagtapmv991@gmail.com>
2019-10-12 01:55:50 +0530

    Homepage component added

---
+++ src/Components/Homepage.component.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Homepage = ()=>(
+	<div className = "homepage">
+		<div classsName = "directory">
+			<div className = "directory-item">
+				<div className = "content">
+					<h1>Hats</h1>
+					<span>Shop Now</span>
+				</div>
+			</div>
+		</div>
+		<div classsName = "directory">
+			<div className = "directory-item">
+				<div className = "content">
+					<h1>Sneakers</h1>
+					<span>Shop Now</span>
+				</div>
+			</div>
+		</div>
+		<div classsName = "directory">
+			<div className = "directory-item">
+				<div className = "content">
+					<h1>Jackets</h1>
+					<span>Shop Now</span>
+				</div>
+			</div>
+		</div>
+		<div classsName = "directory">
+			<div className = "directory-item">
+				<div className = "content">
+					<h1>Mens</h1>
+					<span>Shop Now</span>
+				</div>
+			</div>
+		</div>
+		<div classsName = "directory">
+			<div className = "directory-item">
+				<div className = "content">
+					<h1>Womens</h1>
+					<span>Shop Now</span>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+export default Homepage;
