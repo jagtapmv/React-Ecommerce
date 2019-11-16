69d97b306beaf24ca923ab0c14f19bbe2977e808
jagtapmv <jagtapmv991@gmail.com>
2019-11-16 20:38:38 +0530

    Updates

---
+++ precache-manifest.1a1ec495332ffb423cfebaea2cd39dc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa34819b4eee8adc83a87f167f016f4a",
+    "url": "/React-Ecommerce/index.html"
+  },
+  {
+    "revision": "bd971b7ac92ab5a22a2c",
+    "url": "/React-Ecommerce/static/css/main.7fbe05c7.chunk.css"
+  },
+  {
+    "revision": "0e907cecf3a18be90198",
+    "url": "/React-Ecommerce/static/js/2.9ffa5a63.chunk.js"
+  },
+  {
+    "revision": "bd971b7ac92ab5a22a2c",
+    "url": "/React-Ecommerce/static/js/main.dae0c9fe.chunk.js"
+  },
+  {
+    "revision": "483f65ea6f64979b9761",
+    "url": "/React-Ecommerce/static/js/runtime-main.5f0e618f.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/React-Ecommerce/static/media/crown.ed28accb.svg"
+  }
+]);
